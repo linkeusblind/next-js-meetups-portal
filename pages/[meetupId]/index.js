9efc68e8546471fc0c55d2d1828234758ca07d55
Linkeus <53565230+linkeusblind@users.noreply.github.com>
2021-11-15 17:54:19 +0100

    Update index.js

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -47,7 +47,7 @@ export async function getStaticProps(context) {
     const meetupId = context.params.meetupId;
 
     const client = await MongoClient.connect(
-        "mongodb+srv://dariusz:BolilolTio2110@cluster0.lm99n.mongodb.net/meetups?retryWrites=true&w=majority"
+        "mongodb+srv://mongodb.net/meetups?retryWrites=true&w=majority"
     );
     const db = client.db();
 
